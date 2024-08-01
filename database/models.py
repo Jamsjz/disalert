3d74e50555354f37416ea85c810120ec781d753a
Your Name <gj984233@gmail.com>
2024-08-01 22:12:37 +0545

    sqlite removed

--- database/models.py
+++ database/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-DATABASE_URL = "sqlite://sqlite.db"
+DATABASE_URL = "sqlite:///sqlite.db"
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
