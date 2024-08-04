1beb82f5ac737878b3bc09fcd1b381db140d4f21
Llyr4472 <mr.prashant2077@gmail.com>
2024-08-04 12:39:14 +0545

    update map.py

--- maps/map.py
+++ maps/map.py
@@ -1,17 +1,38 @@
 import folium
-from folium.plugins import HeatMap
-
+from folium.plugins import MarkerCluster, HeatMap
 
 def generate_map(alerts_df):
     map_center = [27.7172, 85.3240]
-    folium_map = folium.Map(location=map_center, zoom_start=12)
+    folium_map = folium.Map(location=map_center, zoom_start=12, tiles="CartoDB positron", zoom_control=False, attribution_control=False)
+
+    # Add satellite layer
+    folium.TileLayer(
+        tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr='Esri',
+        name='Satellite',
+        overlay=False,
+        control=True
+    ).add_to(folium_map)
+
+    # Create a MarkerCluster
+    marker_cluster = MarkerCluster().add_to(folium_map)
+
+    # Custom icons for different disaster types
+    icons = {
+        "Earthquake": "fa-solid fa-house-crack",
+        "Flood": "fa-solid fa-water",
+        "Fire": "fa-solid fa-fire",
+        "Landslide": "fa-solid fa-mountain",
+        "Other": "fa-solid fa-triangle-exclamation"
+    }
 
     # Add markers for each alert
     for _, alert in alerts_df.iterrows():
+        icon = folium.Icon(color="red", icon=icons.get(alert['type'], "info-sign"), prefix='fa')
         folium.Marker(
             location=[alert["lat"], alert["lon"]],
             popup=f"Type: {alert['type']}<br>Description: {alert['description']}<br>Time: {alert['time']}",
-            icon=folium.Icon(color="red", icon="info-sign"),
-        ).add_to(folium_map)
+            icon=icon,
+        ).add_to(marker_cluster)
 
     return folium_map
